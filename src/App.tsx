cec4654881d2b9f897d651267b1fb5b0a32cba6a
Akinola Ayobami <ayobamiakinola84@gmail.com>
2024-11-02 18:29:54 +0100

    feat: Second Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/homepage";
 import "./App.css";
 
 function App() {
 	return (
-		<>
-			<p>SHOPERY</p>
-		</>
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				{/* Add other routes here as needed */}
+			</Routes>
+		</Router>
 	);
 }
 
