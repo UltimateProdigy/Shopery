089d4ec04f04f2133fac2cf5890e76c3e64f94a4
Akinola Ayobami <ayobamiakinola84@gmail.com>
2024-11-19 08:07:26 +0100

    feat: navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,9 +1,11 @@
 import { Logo } from "../logo";
-import { Navlink1 } from "../typography/headers";
+import Cart from "../cart";
+import SearchBox from "../searchbox";
+import Navlinks from "./navlinks";
 
 export default function Navbar() {
 	return (
-		<div>
+		<div className="px-4">
 			<div className="flex justify-between">
 				<div className="flex gap-1">
 					<img
@@ -11,18 +13,32 @@ export default function Navbar() {
 						src="/img/location.svg"
 						alt="location"
 					/>
-					<p className="text-gray-400 text-[10px] mt-3">
+					<p className="text-gray-400 text-[10px] mt-1.5">
 						Store Location: Lincoln- 344, Illinois, Chicago, USA
 					</p>
 				</div>
 				<div className="flex gap-1">
-					<Navlink1 text="ENG" />
-					<Navlink1 text="USD" />
+					<p className="text-gray-400 text-[12px] mt-2 mr-4 cursor-pointer">
+						ENG
+					</p>
+					<p className="text-gray-400 text-[12px] mt-2 mr-3 cursor-pointer">
+						USD
+					</p>
+					<p className="text-gray-400 text-[12px] mt-2 mr-2">|</p>
+					<p className="text-gray-400 text-[12px] mt-2 cursor-pointer">
+						SIGN IN/ SIGN UP
+					</p>
 				</div>
 			</div>
-			<hr className="mt-2" />
-
-			<Logo />
+			<hr className="mt-2 mb-2" />
+			<div className="flex justify-between">
+				<Logo />
+				<SearchBox />
+				<Cart amount={0} value={0} />
+			</div>
+			<div className="mt-3">
+				<Navlinks />
+			</div>
 		</div>
 	);
 }
