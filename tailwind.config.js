e94fee6170c15e0391aa0ea1bd5f28e130b408f6
Akinola Ayobami <ayobamiakinola84@gmail.com>
2024-12-06 15:41:49 +0100

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import animate from "tailwindcss-animate"
 export default {
     darkMode: ["class"],
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -12,5 +13,5 @@ export default {
     		colors: {}
     	}
     },
-	plugins: [require("tailwindcss-animate")],
+	plugins: [animate],
 };
